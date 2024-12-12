bc2640ef416979dec78d67950c4e93d489cac17b
USER <USER@192.168.219.104>
2024-12-13 01:38:53 +0900

    MemberVO 생성 및 작성, LoginVO 수정 후 커밋.

--- legoit/src/main/java/com/legoit/member/vo/MemberVO.java
+++ legoit/src/main/java/com/legoit/member/vo/MemberVO.java
@@ -5,19 +5,19 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-public class LoginVO {
+public class MemberVO {
 
 	private String email; // 아이디 (이메일 형식)
 	private String pw; // 비밀번호
 	private String name; // 이름
-	private String nickName; // 닉네임
+	private String birth; // 생년월일
+	private String tel; // 전화번호
+	private Date regDate; // 가입일
+	private Date conDate; // 최근접속일
+	private String status; // 상태
 	private String photo; // 회원사진
 	private Integer gradeNo; // 등급번호
 	private String gradeName; // 등급명 - grade 테이블에 존재
-	private String status; // 상태
-	private String tel; // 전화번호
-	private Date birth; // 생년월일
+	private String nickName; // 닉네임
 	private String gender; // 성별
-	
-	
 }
